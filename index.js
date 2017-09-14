f9be32ed1a1b7a750affb8d09f66fe549767e81a
MATT FILBERT <mcfilbert@email.wm.edu>
2017-09-14 18:21:30 -0400

    lolol it works

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ io.on('connection', (socket) => {
     console.log('message: ' + msg)
     io.emit('chat message', msg)
   })
+  socket.on('chat message2', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message2', msg)
+  })
+  socket.on('chat message3', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message3', msg)
+  })
+  socket.on('chat message4', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message4', msg)
+  })
+  socket.on('chat message5', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message5', msg)
+  })
+  socket.on('chat message6', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message6', msg)
+  })
+  socket.on('chat message7', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message7', msg)
+  })
   socket.on('disconnect', () => {
     console.log('user DCd')
   })
